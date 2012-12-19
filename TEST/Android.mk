228b159ba3720baf92179249fd842394f4dbf12d
Arun Ravindran <arun.ravindran@intel.com>
2012-12-19 17:27:40 +0200

    rapid_ril: Fix Compilation issue with RILTest
    BZ: 75957
    
    This compilation issue is due to the JB 4.2 change in framework
    classes. In JB 4.2, the telephony classes are part of telephony-common
    JAVA library and this should be included in ther Android.mk file
    for succesful compilation.
    
    Change-Id: Iceb98c9be5ec8b9736604a13b799cd2ee249a289
    Signed-off-by: Arun Ravindran <arun.ravindran@intel.com>
    Reviewed-on: http://android.intel.com:8080/82934
    Reviewed-by: Lucas, Guillaume <guillaume.lucas@intel.com>
    Reviewed-by: Badrappan, Jeevaka <jeevaka.badrappan@intel.com>
    Reviewed-by: Fremaux, Pascal <pascal.fremaux@intel.com>
    Reviewed-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- TEST/Android.mk
+++ TEST/Android.mk
@@ -6,7 +6,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-
+LOCAL_JAVA_LIBRARIES := telephony-common
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := RILTest
