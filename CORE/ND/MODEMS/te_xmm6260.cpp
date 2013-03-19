ee2f48f5c417e1116d190fd4504ff2870393ab87
Arun Ravindran <arun.ravindran@intel.com>
2013-03-19 18:16:50 +0200

    rapid_ril: Handling request type in Deactivate data
    BZ: 90105
    
    This patch implements the usage of the Disconnect reason
    parameter in RIL_REQUEST_DEACTIVATE_DATA_CALl.
    
    If the reason is radio shutdown, the request is completed
    immediately without sending any AT command to modem. The PDP
    cleanup will be done during radio power off.
    
    Change-Id: Id5e3843f0afa2417b485ede390f664cdf89233ee
    Signed-off-by: Arun Ravindran <arun.ravindran@intel.com>
    Reviewed-on: http://android.intel.com:8080/97362
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Fremaux, Pascal <pascal.fremaux@intel.com>
    Reviewed-by: Lucas, Guillaume <guillaume.lucas@intel.com>
    Reviewed-by: Badrappan, Jeevaka <jeevaka.badrappan@intel.com>
    Reviewed-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- CORE/ND/MODEMS/te_xmm6260.cpp
+++ CORE/ND/MODEMS/te_xmm6260.cpp
@@ -2073,6 +2073,13 @@ RIL_RESULT_CODE CTE_XMM6260::CoreDeactivateDataCall(REQUEST_DATA& rReqData,
             RIL_LOG_CRITICAL("CTE_XMM6260::CoreDeactivateDataCall() - pszReason was NULL\r\n");
             goto Error;
         }
+        if (strlen(pszReason) == 1 && pszReason[0] == '1')
+        {
+            // complete the request without sending the AT command to modem.
+            res = RRIL_RESULT_OK_IMMEDIATE;
+            RIL_onRequestComplete(*(RIL_Token*)rReqData.pContextData, RIL_E_SUCCESS, NULL, 0);
+            goto Error;
+        }
         RIL_LOG_INFO("CTE_XMM6260::CoreDeactivateDataCall() - pszReason=[%s]\r\n", pszReason);
     }
 
