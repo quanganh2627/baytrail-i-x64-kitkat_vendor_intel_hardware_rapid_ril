66f03a6416287fc66eb068e788b7b8b0cef43d40
Guillaume Ranquet <guillaumex.ranquet@intel.com>
2014-03-21 17:52:50 +0100

    [CHT] Update repository7260 for ssic
    BZ: 172069
    
    update the 7260 repository for ssic
    
    Change-Id: Id9f3361d8bba9aff71d7ead72b6a6fbb6a0c1f05
    Signed-off-by: Guillaume Ranquet <guillaumex.ranquet@intel.com>

--- config/7260/Android.mk
+++ config/7260/Android.mk
@@ -9,9 +9,18 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := rril_7260_conf_2.txt
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/telephony
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := rril_7260_cfg
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES :=\
     rril_7260_conf_1.txt\
+    rril_7260_conf_2.txt\
 
 include $(BUILD_PHONY_PACKAGE)
