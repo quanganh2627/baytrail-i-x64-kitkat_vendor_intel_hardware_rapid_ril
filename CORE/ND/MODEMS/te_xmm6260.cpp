00f4d065a1b3392c636e6572185a6074d34f03ef
Muratory Pierre <pierre.muratory@intel.com>
2014-06-18 11:55:40 +0200

    Revert "Modify the channel used for sending AT commands using oem hooks"
    BZ: 202854
    
    remove and use the default like it was in WW23 so that there
    is no race condition with OEM channel. This dev will be
    revert as soon as clean interface + dlc defined for
    Coexistence development.
    
    This reverts commit 06d66df9c5261c2c48db5d6284ff1d1529e24688.
    this reverts patch https://android.intel.com/#/c/203216/
    
    Change-Id: I6a14c1beb85a5dec46b152ab3d34ae70ad6571dd
    Signed-off-by: Muratory Pierre <pierre.muratory@intel.com>

--- CORE/ND/MODEMS/te_xmm6260.cpp
+++ CORE/ND/MODEMS/te_xmm6260.cpp
@@ -2351,8 +2351,8 @@ RIL_RESULT_CODE CTE_XMM6260::CoreHookStrings(REQUEST_DATA& rReqData,
                        "RIL_OEM_HOOK_STRING_SEND_AT - Can't construct szCmd1.\r\n");
                 goto Error;
             }
-            // Send this command on OEM channel.
-            uiRilChannel = RIL_CHANNEL_OEM;
+            // Send this cmd on ATCMD channel
+            uiRilChannel = RIL_CHANNEL_ATCMD;
             res = RRIL_RESULT_OK;
             break;
 
