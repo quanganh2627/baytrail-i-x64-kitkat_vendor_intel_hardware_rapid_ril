64e391aab093c94dbee2237520b2ff8e4d982e01
Petru Muraru <petrux.muraru@intel.com>
2014-07-10 18:46:45 +0200

    rril_config: Add configuration file for ATT variant
    BZ: 210496
    
    Add specific RIL configuration file to support ATT
    additional requirement.
    rril_7260_conf_3 added.
    
    Change-Id: I7913c4c513bf5fbd7e88d75d029a40b45f2f6c97
    Signed-off-by: Petru Muraru <petrux.muraru@intel.com>

--- config/7260/Android.mk
+++ config/7260/Android.mk
@@ -17,10 +17,19 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := rril_7260_conf_3.txt
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/telephony
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := rril_7260_cfg
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES :=\
     rril_7260_conf_1.txt\
     rril_7260_conf_2.txt\
+    rril_7260_conf_3.txt\
 
 include $(BUILD_PHONY_PACKAGE)
